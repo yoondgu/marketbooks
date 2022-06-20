dcbd1ad4cd37ec8dc687b7743e88ea3b52cef84c
jewon <jewonlee91@gmail.com>
2022-06-20 09:42:31 +0900

    0617-jewon 고객센터(1:1 문의 게시글조회)

---
+++ src/main/java/dao/InquiryDao.java
@@ -0,0 +1,53 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.inquiryDto;
+import helper.DaoHelper;
+import vo.Inquiry;
+
+public class InquiryDao {
+
+	private static InquiryDao instance = new InquiryDao();
+	private InquiryDao() {}
+	public static InquiryDao getInstance() {
+		return instance;
+	}
+	
+	private DaoHelper helper = DaoHelper.getInstance();
+	
+	/**
+	 * 모든 1:1 문의글 조회하기 
+	 * @return 
+	 * @throws SQLException
+	 */
+	public List<inquiryDto> getAllInquiries() throws SQLException {
+		String sql = "SELECT I.INQUIRY_NO, I.USER_NO, U.USER_NAME, I.INQUIRY_TITLE, I.INQUIRY_CONTENT, I.INQUIRY_DELETED, I.INQUIRY_CREATED_DATE, I.INQUIRY_UPDATED_DATE, I.INQUIRY_ANSWER_CONTENT,I.INQUIRY_ANSWER_CREATED_DATE, I.INQUIRY_ANSWER_UPDATED_DATE, I.INQUIRY_ANSWER_STATUS "
+				   + "FROM HTA_INQUIRIES I, HTA_USERS U "
+				   + "WHERE I.USER_NO = U.USER_NO ";
+		return helper.selectList(sql, rs-> {
+			inquiryDto inquiry = new inquiryDto();
+					
+			inquiry.setNo(rs.getInt("inquiry_no"));
+			inquiry.setUserNo(rs.getInt("user_no"));
+			inquiry.setUserName(rs.getString("user_name"));
+			inquiry.setTitle(rs.getString("inquiry_title"));
+			inquiry.setContent(rs.getString("inquiry_content"));
+			inquiry.setDeleted(rs.getString("inquiry_deleted"));
+			inquiry.setCreatedDate(rs.getDate("inquiry_created_date"));
+			inquiry.setUpdatedDate(rs.getDate("inquiry_updated_date"));
+			inquiry.setAnswerContent(rs.getString("inquiry_answer_content"));
+			inquiry.setAnswerCreatedDate(rs.getDate("inquiry_answer_created_date"));
+			inquiry.setAnswerUpdatedDate(rs.getDate("inquiry_answer_updated_date"));
+			inquiry.setAnswerStatus(rs.getString("inquiry_answer_status"));
+					
+			return inquiry;
+		});
+
+	}
+}
